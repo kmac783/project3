32c56e4955c82d78d1e525ff7a64b0405a024c12
Graham Squires <graham.squires3@gmail.com>
2020-11-19 16:45:20 -0500

    Book Search first commit

---
+++ project3/src/BookSearch.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+<div class="container-full">
+<nav>
+    <div class="nav-wrapper">
+      <form>
+        <div class="input-field">
+          <input id="search" type="search" required>
+          <label for="search"><i class="material-icons">search</i></label>
+          <!-- I've put "red-text" class to emphasize -->
+          <i class="material-icons red-text">close</i>
+        </div>
+      </form>
+    </div>
+  </nav>
+</div>
